460cf172821f855040320b5de7968986faa33498
Saumya Bhardwaj <raaz13bharadwaj@gmail.com>
2024-01-13 00:00:49 +0530

    added input fields

--- frontend/src/places/pages/NewPlaces.js
+++ frontend/src/places/pages/NewPlaces.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useCallback} from "react";
+
+import './NewPlace.css'
+import Input from "../../shared/components/FormElements/Input";
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/utils/validators";
 
 const NewPlace = () => {
-    return <h2> New Places !</h2>
+
+    const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+    const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+    return <form className="place-form">
+        <Input 
+        id="title"
+        element='input' 
+        type="text" 
+        label="Title"
+        validators = {[VALIDATOR_REQUIRE()]}
+        errorText = 'Please Enter a Valid Title.'
+        onInput = {titleInputHandler}
+        />
+        <Input 
+        id="description"
+        element='textarea' 
+        type="text" 
+        label="Description"
+        validators = {[VALIDATOR_MINLENGTH(5)]}
+        errorText = 'Please Enter a Valid Title.'
+        onInput = {descriptionInputHandler}
+        />
+    </form>
 }
 
 export default NewPlace;
